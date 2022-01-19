a30515804b3c61f9583256f986892ddc6121cbcb
Kevin Martin <dev.kmartin@gmail.com>
2022-01-19 14:40:58 +0100

    Refactor pi environment to use heroku api because api hosted is too slow

--- src/environments/environment.prod-pi.ts
+++ src/environments/environment.prod-pi.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  dartsMatcherApiUrl: 'https://api.dartsmatcher.kmartin.nl',
-  dartsMatcherWebsocketUrl: 'wss://api.dartsmatcher.kmartin.nl',
+  dartsMatcherApiUrl: 'https://darts-matcher-api.herokuapp.com',
+  dartsMatcherWebsocketUrl: 'wss://darts-matcher-api.herokuapp.com',
   clientId: 'darts-matcher-web',
   clientSecret: 'secret',
 };
